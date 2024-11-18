adf98135ffeb4121b9eaa365469897a5a0ea8c1d
Viswajith2003 <viswajithviswa715@gmail.com>
2024-11-18 12:38:34 +0530

    about page structure created

--- src/components/about/index.jsx
+++ src/components/about/index.jsx
@@ -4,98 +4,57 @@ import Link from "next/link";
 
 const AboutDetails = () => {
   return (
-    <section className="py-20 w-full">
-      <div className="grid grid-cols-12 gap-4 xs:gap-6  md:gap-8 w-full">
-        <ItemLayout
-          className={
-            " col-span-full lg:col-span-8 row-span-2 flex-col items-start"
-          }
-        >
-          <h2 className="  text-xl md:text-2xl text-left w-full capitalize">
-            Architect of Enchantment
-          </h2>
-          <p className="font-light  text-xs sm:text-sm md:text-base   ">
-            My journey in web development is powered by an array of mystical
-            tools and languages, with JavaScript casting the core of my
-            enchantments. I wield frameworks like React.js and Next.js with
-            precision, crafting seamless portals (websites) that connect realms
-            (users) across the digital universe. The ancient arts of the
-            Jamstack empower me to create fast, secure, and dynamic experiences,
-            while my design skills ensure every creation is not only functional
-            but visually captivating. Join me as I continue to explore new
-            spells and technologies to shape the future of the web.
-          </p>
-        </ItemLayout>
-
-        <ItemLayout
-          className={" col-span-full xs:col-span-6 lg:col-span-4 text-accent"}
-        >
-          <p className="font-semibold w-full text-left text-2xl sm:text-5xl">
-            25+ <sub className="font-semibold text-base">clients</sub>
-          </p>
-        </ItemLayout>
-
-        <ItemLayout
-          className={"col-span-full xs:col-span-6 lg:col-span-4 text-accent"}
-        >
-          <p className="font-semibold w-full text-left text-2xl sm:text-5xl">
-            4+{" "}
-            <sub className="font-semibold text-base">years of experience</sub>
-          </p>
-        </ItemLayout>
-
-        <ItemLayout
-          className={"col-span-full sm:col-span-6 md:col-span-4 !p-0"}
-        >
-          <img
-            className="w-full h-auto"
-            src={`${process.env.NEXT_PUBLIC_GITHUB_STATS_URL}/api/top-langs?username=codebucks27&theme=transparent&hide_border=true&title_color=FEFE5B&text_color=FFFFFF&icon_color=FEFE5B&text_bold=false`}
-            alt="CodeBucks"
-            loading="lazy"
-          />
-        </ItemLayout>
-
-        <ItemLayout className={"col-span-full md:col-span-8 !p-0"}>
-          <img
-            className="w-full h-auto"
-            src={`${process.env.NEXT_PUBLIC_GITHUB_STATS_URL}/api?username=codebucks27&theme=transparent&hide_border=true&title_color=FEFE5B&text_color=FFFFFF&icon_color=FEFE5B&text_bold=false`}
-            alt="CodeBucks"
-            loading="lazy"
-          />
-        </ItemLayout>
-
-        <ItemLayout className={"col-span-full"}>
-          <img
-            className="w-full h-auto"
-            src={`https://skillicons.dev/icons?i=appwrite,aws,babel,bootstrap,cloudflare,css,d3,docker,figma,firebase,gatsby,git,github,graphql,html,ipfs,js,jquery,kubernetes,linux,mongodb,mysql,netlify,nextjs,nodejs,npm,postgres,react,redux,replit,sass,supabase,tailwind,threejs,vercel,vite,vscode,yarn`}
-            alt="CodeBucks"
-            loading="lazy"
-          />
-        </ItemLayout>
-
-        <ItemLayout className={"col-span-full md:col-span-6 !p-0"}>
-          <img
-            className="w-full h-auto"
-            src={`${process.env.NEXT_PUBLIC_GITHUB_STREAK_STATS_URL}?user=codebucks27&theme=dark&hide_border=true&type=svg&background=EB545400&ring=FEFE5B&currStreakLabel=FEFE5B`}
-            alt="CodeBucks"
-            loading="lazy"
-          />
-        </ItemLayout>
-
-        <ItemLayout className={"col-span-full md:col-span-6 !p-0"}>
-          <Link
-            href="https://github.com/codebucks27/Nextjs-contentlayer-blog"
-            target="_blank"
-            className="w-full"
-          >
+    <section className="w-full m-8">
+      <div className="flex space-x-6 w-full">
+        {/* left part */}
+        <div className="flex flex-col space-y-5 w-1/4">
+          <div className="mb-12">
             <img
-              className="w-full h-auto"
-              src={`${process.env.NEXT_PUBLIC_GITHUB_STATS_URL}/api/pin/?username=codebucks27&repo=Nextjs-contentlayer-blog&theme=transparent&hide_border=true&title_color=FEFE5B&text_color=FFFFFF&icon_color=FEFE5B&text_bold=false&description_lines_count=2`}
-              alt="CodeBucks"
-              loading="lazy"
+              src="/profile-photo.jpg" // Replace with the path to your profile photo
+              alt=""
+              className="ml-10 w-72 h-72 rounded-full border-4 border-yellow-400 hover:border-blue-500 object-cover shadow-lg"
             />
-          </Link>
-        </ItemLayout>
+          </div>
+          <div className="border-2 border-yellow-400 hover:border-blue-500 p-3 rounded-md">
+            <h1>Contacts:</h1>
+            <br />
+            <div className="gap-3 flex">
+              <i>Mail</i>
+              <p>viswajithviswa715@gmail.com</p>
+            </div>
+            <div className="gap-3 flex">
+              <i>Phone</i>
+              <p>+91 9072906576</p>
+            </div>
+          </div>
+          <div className="border-2 border-yellow-400 hover:border-blue-500 p-3 rounded-md">
+            <h1>Let's connect with social:</h1>
+            <br />
+            <div className="gap-3 flex">
+              <i>Youtube</i>
+              <i>Instagram</i>
+              <i>LinkedIn</i>
+            </div>
+          </div>
+        </div>
+
+        {/* right part */}
+        <div className="flex flex-1 space-x-4">
+          <div className="flex flex-col space-y-4 w-2/3">
+            <div className="border-2 border-yellow-400 hover:border-blue-500 p-3 rounded-md h-1/3">
+              Description
+            </div>
+            <div className="border-2 border-yellow-400 hover:border-blue-500 p-3 rounded-md h-full">
+              Tech skills
+            </div>
+          </div>
+          <div className="border-2 border-yellow-400 hover:border-blue-500 p-3 rounded-md w-1/3">
+            Education
+          </div>
+        </div>
+      </div>
+      <div className="border-2 border-yellow-400 hover:border-blue-500 p-3 rounded-md w-full mt-8 h-40">
+        <h4>Tech stack:</h4>
       </div>
     </section>
   );
