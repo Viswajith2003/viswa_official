c0db2468b88803a7b000a28b4460bcc15ed67090
Viswajith2003 <viswajithviswa715@gmail.com>
2024-11-19 05:26:38 +0530

    contact page full completed

--- src/components/contact/Form.jsx
+++ src/components/contact/Form.jsx
@@ -1,9 +1,9 @@
 "use client";
-import React from "react";
-import { useForm } from "react-hook-form";
-import emailjs from "@emailjs/browser";
-import { Toaster, toast } from "sonner";
 import { motion } from "framer-motion";
+import React from "react";
+import { FaLinkedin } from "react-icons/fa";
+import { BsInstagram, BsGithub } from "react-icons/bs";
+import { AiFillYoutube } from "react-icons/ai";
 
 const container = {
   hidden: { opacity: 0 },
@@ -11,150 +11,78 @@ const container = {
     opacity: 1,
     transition: {
       staggerChildren: 0.3,
-      delayChildren: 0.2,
     },
   },
 };
 
-const item = {
-  hidden: { scale: 0 },
-  show: { scale: 1 },
-};
-
-export default function Form() {
-  const {
-    register,
-    handleSubmit,
-    formState: { errors },
-  } = useForm();
-
-  const sendEmail = (params) => {
-    const toastId = toast.loading("Sending your message, please wait...");
-
-    toast.info(
-      "Form submissions are demo-only here. Please checkout the final code repo to enable it. If you want to connect you can reach out to me via codebucks27@gmail.com.",
-      {
-        id: toastId,
-      }
-    );
-
-    // comment out the above toast.info and uncomment the below code to enable emailjs
-
-    // emailjs
-    //   .send(
-    //     process.env.NEXT_PUBLIC_SERVICE_ID,
-    //     process.env.NEXT_PUBLIC_TEMPLATE_ID,
-    //     params,
-    //     {
-    //       publicKey: process.env.NEXT_PUBLIC_PUBLIC_KEY,
-    //       limitRate: {
-    //         throttle: 5000, // you can not send more then 1 email per 5 seconds
-    //       },
-    //     }
-    //   )
-    //   .then(
-    //     () => {
-    //       toast.success(
-    //         "I have received your message, I will get back to you soon!",
-    //         {
-    //           id: toastId,
-    //         }
-    //       );
-    //     },
-    //     (error) => {
-    //       // console.log("FAILED...", error.text);
-    //       toast.error(
-    //         "There was an error sending your message, please try again later!",
-    //         {
-    //           id: toastId,
-    //         }
-    //       );
-    //     }
-    //   );
-  };
-
-  const onSubmit = (data) => {
-    const templateParams = {
-      to_name: "CodeBucks",
-      from_name: data.name,
-      reply_to: data.email,
-      message: data.message,
-    };
-
-    sendEmail(templateParams);
-  };
-
+export default function Forms() {
   return (
-    <>
-      <Toaster richColors={true} />
-      <motion.form
-        variants={container}
-        initial="hidden"
-        animate="show"
-        onSubmit={handleSubmit(onSubmit)}
-        className="max-w-md w-full flex flex-col items-center justify-center space-y-4"
-      >
-        <motion.input
-          variants={item}
-          type="text"
-          placeholder="name"
-          {...register("name", {
-            required: "This field is required!",
-            minLength: {
-              value: 3,
-              message: "Name should be atleast 3 characters long.",
-            },
-          })}
-          className="w-full p-2 rounded-md shadow-lg text-foreground focus:outline-none focus:ring-2 focus:ring-accent/50 custom-bg"
-        />
-        {errors.name && (
-          <span className="inline-block self-start text-accent">
-            {errors.name.message}
-          </span>
-        )}
-        <motion.input
-          variants={item}
-          type="email"
-          placeholder="email"
-          {...register("email", { required: "This field is required!" })}
-          className="w-full p-2 rounded-md shadow-lg text-foreground focus:outline-none focus:ring-2 focus:ring-accent/50 custom-bg"
-        />
-        {errors.email && (
-          <span className="inline-block self-start text-accent">
-            {errors.email.message}
-          </span>
-        )}
-        <motion.textarea
-          variants={item}
-          placeholder="message"
-          {...register("message", {
-            required: "This field is required!",
-            maxLength: {
-              value: 500,
-              message: "Message should be less than 500 characters",
-            },
-            minLength: {
-              value: 50,
-              message: "Message should be more than 50 characters",
-            },
-          })}
-          className="w-full p-2 rounded-md shadow-lg text-foreground focus:outline-none focus:ring-2 focus:ring-accent/50 custom-bg"
-        />
-        {errors.message && (
-          <span className="inline-block self-start text-accent">
-            {errors.message.message}
-          </span>
-        )}
-
-        <motion.input
-          variants={item}
-          value="Cast your message!"
-          className="px-10 py-4 rounded-md shadow-lg bg-background border border-accent/30 border-solid
-      hover:shadow-glass-sm backdrop-blur-sm text-foreground focus:outline-none focus:ring-2 focus:ring-accent/50 cursor-pointer capitalize
-      "
-          type="submit"
-        />
-      </motion.form>
-    </>
+    <motion.section
+      initial={{ scale: 0 }}
+      whileInView={{ scale: 1 }}
+      transition={{ duration: 0.5 }}
+      viewport={{ once: true }}
+      id="contact"
+      className="pb-16 container mx-auto md:ml-20"
+    >
+      <div className="flex flex-col md:flex-row justify-between items-center">
+        <motion.div
+          variants={container}
+          className="card p-3 rounded-lg h-auto text-black space-y-4 mx-auto md:ml-8 md:mr-4"
+        >
+          <div className="space-y-6 mt-8 text-white w-full md:w-96">
+            <input
+              type="text"
+              placeholder="Name :"
+              className="border-2 border-white p-3 rounded-md h-12 w-full bg-transparent"
+            />
+            <input
+              type="email"
+              placeholder="Email :"
+              className="border-2 border-white p-3 rounded-md h-12 w-full bg-transparent"
+            />
+            <textarea
+              name="message"
+              placeholder="Message :"
+              className="border-2 border-white p-3 rounded-md h-40 md:h-52 w-full bg-transparent"
+            ></textarea>
+          </div>
+          <div className="flex justify-between items-center">
+            <motion.div
+              variants={container}
+              className="flex space-x-5 mt-3 text-white border-[1px] rounded-lg p-[6px] text-center"
+            >
+              <BsGithub className="w-6  h-6  hover:scale-95" />
+              <BsInstagram className="w-6 h-6 hover:scale-95" />
+              <FaLinkedin className="w-6  h-6  hover:scale-95" />
+              <AiFillYoutube className="w-8 h-8 hover:scale-95 " />
+            </motion.div>
+            <button className="p-2 w-36 mt-3 font-bold text-white rounded-lg border-[1px] hover:scale-95">
+              Submit
+            </button>
+          </div>
+        </motion.div>
+        <motion.div
+          initial={{ scale: 0 }}
+          whileInView={{ scale: 1 }}
+          transition={{ duration: 0.5 }}
+          viewport={{ once: true }}
+          className="w-full md:w-[540px] h-[300px] sm:h-[450px] rounded-lg bg-slate-900 mx-auto mt-8 md:mt-0"
+        >
+          <iframe
+            title="google-maps"
+            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d25869.703228616378!2d75.84910382798228!3d11.088537940849525!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3ba6522474222987%3A0x2de01cb6b87aafe5!2sKoottumoochi!5e0!3m2!1sen!2sin!4v1708170014170!5m2!1sen!2sin"
+            width="600"
+            height="450"
+            style={{ border: 0 }}
+            allowFullScreen=""
+            loading="lazy"
+            referrerPolicy="no-referrer-when-downgrade"
+            className="border-0 w-full h-full rounded-lg"
+          ></iframe>
+        </motion.div>
+      </div>
+    </motion.section>
   );
 }
+
